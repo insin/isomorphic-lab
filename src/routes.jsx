dd770013feb9e2a24c2a7cea90e93ed79ba83a71
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-12-18 20:44:02 +0000

    Made NotFoundRoute a sibling of the App route

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,8 +5,10 @@ var {NotFoundRoute, Route} = require('react-router')
 
 var {AddThing, App, NotFound, Things} = require('./components')
 
-module.exports = <Route name="home" path="/" handler={App}>
-  <Route name="things" handler={Things}/>
-  <Route name="addthing" handler={AddThing}/>
+module.exports = [
+  <Route name="home" path="/" handler={App}>
+    <Route name="things" handler={Things}/>
+    <Route name="addthing" handler={AddThing}/>
+  </Route>,
   <NotFoundRoute name="notfound" handler={NotFound}/>
-</Route>
+]
