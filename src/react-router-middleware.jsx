726a72409e6d6c8bd704d238a2f9b3304047b3ce
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-12-21 14:04:35 +0000

    Extracted a react-router-middleware module Use a 'react-404' view for its 404 rendering

---
+++ src/react-router-middleware.jsx
@@ -0,0 +1,91 @@
+'use strict';
+
+var querystring = require('querystring')
+
+var assign = require('react/lib/Object.assign')
+var React = require('react')
+var Router = require('react-router')
+var Redirect = require('react-router/modules/utils/Redirect')
+
+var fetchData = require('./utils/fetchData')
+var Render = require('./utils/Render')
+
+module.exports = function(routes) {
+  if (!routes) {
+    throw new Error('Routes must be provided')
+  }
+
+  function renderApp(location, extraProps, cb) {
+    var router = Router.create({
+      location,
+      routes,
+
+      onAbort(reason) {
+        if (reason instanceof Error) {
+          cb(reason)
+        }
+        else if (reason instanceof Redirect) {
+          cb(null, {redirect: reason})
+        }
+        else if (reason instanceof Render) {
+          cb(null, {render: reason})
+        }
+        else {
+          cb(null, reason)
+        }
+      },
+
+      onError(err) {
+        cb(err)
+      }
+    })
+
+    router.run((Handler, state) => {
+      if (state.routes[0].name == 'notfound') {
+        var html = React.renderToStaticMarkup(<Handler/>)
+        return cb(null, {notFound: true}, html)
+      }
+      fetchData(state.routes, state.params, (err, props) => {
+        if (extraProps) {
+          assign(props, extraProps)
+        }
+        var html = React.renderToString(<Handler {...props}/>)
+        cb(null, null, html, JSON.stringify(props))
+      })
+    })
+  }
+
+  function renderAppHandler(res, next, err, special, html, props) {
+    if (err) {
+      return next(err)
+    }
+
+    if (!special) {
+      return res.render('react', {html, props})
+    }
+
+    if (special.notFound) {
+      res.status(404).render('react-404', {title: 'Not Found', html})
+    }
+    else if (special.redirect) {
+      res.redirect(303, special.redirect.to)
+    }
+    else if (special.render) {
+      var {url, extraProps} = special.render
+      renderApp(url, extraProps, renderAppHandler.bind(null, res, next))
+    }
+    else {
+      console.error('Unexpected special response case: ', special.constructor, special)
+      next(new Error('Unexpected special response case, see server logs.'))
+    }
+  }
+
+  return function reactRouter(req, res, next) {
+    var url = req.url
+    // Use query params to pass POST data to willTransitionTo
+    if (req.method == 'POST') {
+      url += `?_method=${req.method}&${querystring.stringify(req.body)}`
+    }
+    renderApp(url, null, renderAppHandler.bind(null, res, next))
+  }
+}
--- src/server.jsx
+++ src/server.jsx
@@ -1,7 +1,6 @@
 'use strict';
 
 var path = require('path')
-var querystring = require('querystring')
 
 var bodyParser = require('body-parser')
 var compression = require('compression')
@@ -10,22 +9,16 @@ var express = require('express')
 var favicon = require('serve-favicon')
 var logger = require('morgan')
 var serveStatic = require('serve-static')
-var session = require('express-session')
-
-var assign = require('react/lib/Object.assign')
-var React = require('react')
-var Router = require('react-router')
-var Redirect = require('react-router/modules/utils/Redirect')
+//var session = require('express-session')
 
 var pkg = require('../package.json')
 var api = require('./api')
-var routes = require('./routes')
-var fetchData = require('./utils/fetchData')
-var Render = require('./utils/Render')
+var reactRouter = require('./react-router-middleware')
 
 var app = express()
 app.set('host', process.env.HOST || '0.0.0.0')
 app.set('port', process.env.PORT || 3000)
+app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, '../views'))
 app.use(logger('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -33,83 +26,11 @@ app.use(bodyParser.json())
 app.use(compression())
 app.use(favicon(path.join(__dirname, '../static/favicon.ico')))
 app.use(serveStatic(path.join(__dirname, '../static')))
-app.use(session({secret: process.env.SECRET || 'secret', resave: false, saveUninitialized: true}))
+//app.use(session({secret: process.env.SECRET || 'secret', resave: false, saveUninitialized: true}))
 
 app.use('/api', api)
 
-function renderApp(location, extraProps, cb) {
-  var router = Router.create({
-    location,
-    routes,
-
-    onAbort(reason) {
-      if (reason instanceof Error) {
-        cb(reason)
-      }
-      else if (reason instanceof Redirect) {
-        cb(null, {redirect: reason})
-      }
-      else if (reason instanceof Render) {
-        cb(null, {render: reason})
-      }
-      else {
-        cb(null, reason)
-      }
-    },
-
-    onError(err) {
-      cb(err)
-    }
-  })
-
-  router.run((Handler, state) => {
-    if (state.routes[0].name == 'notfound') {
-      var html = React.renderToStaticMarkup(<Handler/>)
-      return cb(null, {notFound: true}, html)
-    }
-    fetchData(state.routes, state.params, (err, props) => {
-      if (extraProps) {
-        assign(props, extraProps)
-      }
-      var html = React.renderToString(<Handler {...props}/>)
-      cb(null, null, html, JSON.stringify(props))
-    })
-  })
-}
-
-function renderAppHandler(res, next, err, special, html, props) {
-  if (err) {
-    return next(err)
-  }
-
-  if (!special) {
-    return res.render('react.jade', {html, props})
-  }
-
-  if (special.notFound) {
-    res.status(404).render('base.jade', {title: 'Not Found', html})
-  }
-  else if (special.redirect) {
-    res.redirect(303, special.redirect.to)
-  }
-  else if (special.render) {
-    var {url, extraProps} = special.render
-    renderApp(url, extraProps, renderAppHandler.bind(null, res, next))
-  }
-  else {
-    console.error('Unexpected special response case: ', special.constructor, special)
-    next(new Error('Unexpected special response case, see server logs.'))
-  }
-}
-
-app.use((req, res, next) => {
-  var url = req.url
-  // Use query params to pass POST data to willTransitionTo
-  if (req.method == 'POST') {
-    url += `?_method=${req.method}&${querystring.stringify(req.body)}`
-  }
-  renderApp(url, null, renderAppHandler.bind(null, res, next))
-})
+app.use(reactRouter(require('./routes')))
 
 if ('development' == app.get('env')) {
   app.use(errorhandler())
