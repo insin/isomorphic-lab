11678d2a34ebe4ad296edccdce09704826242e24
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-12-24 01:10:45 +0000

    Client doesn't need the host & port when the API is on the same host

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,12 @@
 'use strict';
 
+var env = require('./utils/env')
+
 var HOST = process.env.HOST
 var PORT = process.env.PORT
-var BASE_URL = `http://${HOST}:${PORT}`
+var BASE_URL = env.SERVER ? `http://${HOST}:${PORT}` : ''
 
 module.exports = {
   API_URL: `${BASE_URL}/api`
-, BASE_URL
-, HOST
-, PORT
 , TITLE: 'Isomorphic Lab'
 }
