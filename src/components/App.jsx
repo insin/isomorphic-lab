56c1ac6465633a620bed2870e833a46154d81663
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-12-24 01:09:13 +0000

    Add a GutHub ribbon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,6 +27,14 @@ var App = React.createClass({
         <Link to="things">Things</Link>
       </nav>
       <RouteHandler {...this.props}/>
+      <footer>
+        <a href="https://github.com/insin/isomorphic-lab">
+          <img
+            style={{position: 'absolute', top: 0, right: 0, border: 0}}
+            src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"
+          />
+        </a>
+      </footer>
     </div>
   }
 })
