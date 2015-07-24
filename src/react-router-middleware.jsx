c8e727840744589f081870c84ffbf387603dd181
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-07-24 18:10:25 +0100

    Reorganise middleware

--- src/react-router-middleware.jsx
+++ src/react-router-middleware.jsx
@@ -11,76 +11,48 @@ var getTitle = require('./utils/getTitle')
 var getPropsForComponent = require('./utils/getPropsForComponent')
 
 module.exports = function(routes, options) {
-  if (!routes) {
-    throw new Error('Routes must be provided')
-  }
+  if (!routes) { throw new Error('Routes must be provided.') }
   options = assign({title: {}}, options)
 
-  function renderApp(location, cb) {
+  return function reactRouterMiddleware(req, res, next) {
+    // Provide the method and body of non-GET requests as location state
+    var state = (req.method !== 'GET' ? {method: req.method, body: req.body} : null)
+    route(new Location(req.url, req.query, state), res, next)
+  }
+
+  function route(location, res, next) {
     Router.run(routes, location, (err, routerState, transition) => {
-      if (err) { return cb(err) }
+      if (err) { return next(err) }
 
       if (transition.isCancelled) {
-        return cb(null, transition)
+        if (transition.redirectInfo) {
+          var {pathname, query, state} = transition.redirectInfo
+          // Re-run routing for redirect locations which have non-URL state
+          if (state) {
+            route(new Location(pathname, query, state), res, next)
+          }
+          else {
+            res.redirect(303, pathname)
+          }
+        }
+        else {
+          console.error('Transition cancelled in an unexpected way: ', transition.abortReason)
+          next(new Error('Transition cancelled in an unexpected way.'))
+        }
+        return
       }
 
       AsyncProps.hydrate(routerState, (err, {propsArray, componentsArray}) => {
-        if (err) { return cb(err) }
+        if (err) { return next(err) }
         function createElement(Component, props) {
           var asyncProps = getPropsForComponent(Component, componentsArray, propsArray)
           return <Component {...asyncProps} {...props}/>
         }
-        var html = React.renderToString(
-          <Router {...routerState} createElement={createElement}/>
-        )
-        try {
-          var title = getTitle(routerState, componentsArray, propsArray, options.title)
-          cb(null, transition, html, JSON.stringify(propsArray), title)
-        }
-        catch(err) {
-          cb(err)
-        }
+        var html = React.renderToString(<Router {...routerState} createElement={createElement}/>)
+        var title = getTitle(routerState, componentsArray, propsArray, options.title)
+        res.render('react', {html, title, props: JSON.stringify(propsArray)})
       })
     })
   }
-
-  function renderAppHandler(res, next, err, transition, html, props, title) { // ಠ_ಠ
-    if (err) {
-      return next(err)
-    }
-
-    if (!transition.isCancelled) {
-      return res.render('react', {title, html, props})
-    }
-
-    if (transition.redirectInfo) {
-      var {pathname, query, state} = transition.redirectInfo
-      // Rather than introducing a server-specific abort reason object, use the
-      // fact that a redirect has non-URL state as an indication that a response
-      // should be rendered directly.
-      if (state) {
-        renderApp(
-          new Location(pathname, query, state),
-          renderAppHandler.bind(null, res, next)
-        )
-      }
-      else {
-        res.redirect(303, pathname)
-      }
-    }
-    else {
-      console.error('Transiton cancelled in an unexpected way: ', transition.abortReason)
-      next(new Error('Transiton cancelled in an unexpected way, see server logs.'))
-    }
-  }
-
-  return function reactRouter(req, res, next) {
-    // Provide the method and body of non-GET requests as a request-like object
-    var state = null
-    if (req.method !== 'GET') {
-      state = {method: req.method, body: req.body}
-    }
-    var location = new Location(req.url, req.query, state)
-    renderApp(location, renderAppHandler.bind(null, res, next))
-  }
 }
+
